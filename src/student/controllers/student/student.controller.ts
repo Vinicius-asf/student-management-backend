f85747ca996b77c195dcd248b0f231f614cb2251
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-02 20:34:02 -0300

    feat: student controller and service for get route

--- src/student/controllers/student/student.controller.ts
+++ src/student/controllers/student/student.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { StudentService } from 'src/student/services/student/student.service';
+import { ListAllEntitiesPaginated } from 'src/student/dto/student/listAllEntitiesPaginated.dto';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+  constructor(private studentService: StudentService) {}
+
+  @Get()
+  public async find(
+    @Param('page') page: number,
+    @Param('count') count: number,
+  ): Promise<ListAllEntitiesPaginated> {
+    return this.studentService.find(page, count);
+  }
+}
--- src/student/services/student/student.service.ts
+++ src/student/services/student/student.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from 'src/app.entities';
+import { Repository } from 'typeorm';
+import { ListAllEntitiesPaginated } from 'src/student/dto/student/listAllEntitiesPaginated.dto';
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+  ) {}
+
+  public async find(
+    page: number,
+    count: number,
+  ): Promise<ListAllEntitiesPaginated> {
+    const response: Student[] = await this.studentRepository.find({
+      skip: (page - 1) * count,
+      take: count,
+    });
+    return {
+      page: page,
+      items: response,
+    };
+  }
+}
