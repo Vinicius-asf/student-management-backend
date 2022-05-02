3f9f4936da8c404d15b693d13aff6ab5fcb4cc0a
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-02 16:27:15 -0300

    fix: student entity inside its module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentController } from './controllers/student/student.controller';
 import { StudentService } from './services/student/student.service';
-
+import { Student } from './student.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Student])],
   controllers: [StudentController],
   providers: [StudentService],
 })
