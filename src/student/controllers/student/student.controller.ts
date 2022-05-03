d6168794ddd565b620fec87d4598627a25c0abc2
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-03 11:05:08 -0300

    feat: cpf validation pipe

--- src/student/controllers/student/student.controller.ts
+++ src/student/controllers/student/student.controller.ts
@@ -3,6 +3,7 @@ import { StudentService } from 'src/student/services/student/student.service';
 import { ListAllEntitiesPaginated } from 'src/student/dto/student/listAllEntitiesPaginated.dto';
 import { CreateStudent } from 'src/student/dto/student/createStudent.dto';
 import { PaymentMethodsType } from 'src/student/student.entity';
+import { CPFValidationPipe } from 'src/pipes/cpfValidation/cpfValidation.pipe';
 
 @Controller('student')
 export class StudentController {
@@ -19,7 +20,7 @@ export class StudentController {
   @Post()
   public async create(
     @Body('name') name: string,
-    @Body('cpf') cpf: string,
+    @Body('cpf', CPFValidationPipe) cpf: string,
     @Body('birthdate') birthdate: string,
     @Body('payment_method') payment_method: PaymentMethodsType,
   ): Promise<CreateStudent> {
