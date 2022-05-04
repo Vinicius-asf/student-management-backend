ea967ba0f3ed4ad0a12a8e18ef924df91ef0a26d
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-04 18:57:25 -0300

    chore: enrollment module imports billModule

--- src/enrollment/enrollment.module.ts
+++ src/enrollment/enrollment.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BillModule } from 'src/bill/bill.module';
 import { EnrollmentController } from './controllers/enrollment/enrollment.controller';
 import { Enrollment } from './enrollment.entity';
 import { EnrollmentService } from './services/enrollment/enrollment.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Enrollment])],
+  imports: [TypeOrmModule.forFeature([Enrollment]), BillModule],
   controllers: [EnrollmentController],
   providers: [EnrollmentService],
 })
