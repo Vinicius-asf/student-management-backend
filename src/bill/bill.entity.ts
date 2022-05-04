9dbf6d4a3ba48ab0d59aef342da235f1a735fe67
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-04 16:46:38 -0300

    feat: bill entity

---
+++ src/bill/bill.entity.ts
@@ -0,0 +1,50 @@
+import { IsDate, IsEnum, IsInt, Min } from 'class-validator';
+import { Enrollment } from 'src/app.entities';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+export enum StatusType {
+  'open',
+  'pending',
+  'paid',
+}
+
+@Entity()
+export class Bill {
+  @PrimaryGeneratedColumn()
+  @IsInt()
+  id: number;
+
+  @ManyToOne(() => Enrollment, (enrollment) => enrollment.id, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'enrollment_id' })
+  @IsInt()
+  student: Enrollment;
+
+  @Column({
+    type: 'integer',
+  })
+  @IsInt()
+  @Min(1)
+  amount: number;
+
+  @Column({
+    type: 'date',
+  })
+  @IsDate()
+  due_date: Date;
+
+  @Column({
+    type: 'enum',
+    enum: StatusType,
+    default: 'open',
+  })
+  @IsEnum(StatusType)
+  status: StatusType;
+}
