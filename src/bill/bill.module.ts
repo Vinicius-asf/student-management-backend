385a9f53a5d4af3c58aadd18c9fbeacb2ab0a486
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-04 18:56:37 -0300

    chore: bill module imports repository and exports service

--- src/bill/bill.module.ts
+++ src/bill/bill.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bill } from './bill.entity';
 import { BillService } from './services/bill/bill.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Bill])],
   providers: [BillService],
+  exports: [BillService],
 })
 export class BillModule {}
