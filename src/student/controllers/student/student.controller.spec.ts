53bc57b3b41cb0d2c904805d5c2c5b682524ff21
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-05 18:47:39 -0300

    fix: student controller toBeDefined test

--- src/student/controllers/student/student.controller.spec.ts
+++ src/student/controllers/student/student.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Student } from '../../../app.entities';
+import { mockRepository } from '../../../mock/mockRepository/mockRepository.mock';
+import { StudentService } from '../../services/student/student.service';
 import { StudentController } from './student.controller';
 
 describe('StudentController', () => {
@@ -7,6 +11,13 @@ describe('StudentController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StudentController],
+      providers: [
+        StudentService,
+        {
+          provide: getRepositoryToken(Student),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<StudentController>(StudentController);
