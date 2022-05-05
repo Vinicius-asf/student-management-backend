2ca50d78b2105e2935bac954751a034d430ae815
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-05 18:19:25 -0300

    fix: bill service test toBeDefined

--- src/bill/services/bill/bill.service.spec.ts
+++ src/bill/services/bill/bill.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BillService } from './bill.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Bill } from '../../bill.entity';
+import { mockRepository } from '../../../mock/mockRepository/mockRepository.mock';
 
 describe('BillService', () => {
   let service: BillService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BillService],
+      providers: [
+        BillService,
+        {
+          provide: getRepositoryToken(Bill),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<BillService>(BillService);
