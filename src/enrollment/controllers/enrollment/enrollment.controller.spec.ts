3ba1e60f3427a69b676ee7b37f3180488a5c03d0
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-05 18:42:39 -0300

    fix: enrollment controller toBeDefined test

--- src/enrollment/controllers/enrollment/enrollment.controller.spec.ts
+++ src/enrollment/controllers/enrollment/enrollment.controller.spec.ts
@@ -1,4 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Bill, Enrollment } from '../../../app.entities';
+import { BillService } from '../../../bill/services/bill/bill.service';
+import { mockRepository } from '../../../mock/mockRepository/mockRepository.mock';
+import { EnrollmentService } from '../../services/enrollment/enrollment.service';
 import { EnrollmentController } from './enrollment.controller';
 
 describe('EnrollmentController', () => {
@@ -7,6 +12,18 @@ describe('EnrollmentController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EnrollmentController],
+      providers: [
+        EnrollmentService,
+        {
+          provide: getRepositoryToken(Enrollment),
+          useValue: mockRepository,
+        },
+        BillService,
+        {
+          provide: getRepositoryToken(Bill),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<EnrollmentController>(EnrollmentController);
