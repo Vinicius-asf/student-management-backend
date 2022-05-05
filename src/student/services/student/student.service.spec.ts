8cbb8372f0e6ce1e595285777dc75f9a2cb286ad
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-05 18:38:07 -0300

    fix: student service test toBeDefined

--- src/student/services/student/student.service.spec.ts
+++ src/student/services/student/student.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Student } from '../../../app.entities';
+import { mockRepository } from '../../../mock/mockRepository/mockRepository.mock';
 import { StudentService } from './student.service';
 
 describe('StudentService', () => {
@@ -6,7 +9,13 @@ describe('StudentService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StudentService],
+      providers: [
+        StudentService,
+        {
+          provide: getRepositoryToken(Student),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<StudentService>(StudentService);
