c5492abc8434917c79b9405a2d313dcefe409116
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-06 01:03:02 -0300

    chore: changing database connection to url type

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,7 @@ import entities from './app.entities';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        url: configService.get('DATABASE_URL'),
         entities: entities,
         synchronize: true,
       }),
