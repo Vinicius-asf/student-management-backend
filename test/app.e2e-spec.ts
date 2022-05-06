a76eb352a4ece40e994260ac7e98051033a8a7d7
Vinicius Ferreira <viniciusant3@gmail.com>
2022-05-06 00:25:22 -0300

    feat: e2e for get methods

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,49 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/student (GET)', async () => {
+    const response = await request(app.getHttpServer()).get('/student').send({
+      page: 1,
+      count: 10,
+    });
+    return response.statusCode === HttpStatus.OK;
+  });
+
+  it('/student (GET) without pagination', async () => {
+    const response = await request(app.getHttpServer()).get('/student');
+    return response.statusCode === HttpStatus.BAD_REQUEST;
+  });
+
+  it('/student (GET) with wrong pagination', async () => {
+    const response = await request(app.getHttpServer()).get('/student').send({
+      page: 0,
+      count: 0,
+    });
+    return response.statusCode === HttpStatus.BAD_REQUEST;
+  });
+
+  it('/enrollment (GET)', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/enrollment')
+      .send({
+        page: 1,
+        count: 10,
+      });
+    return response.statusCode === HttpStatus.OK;
+  });
+
+  it('/enrollment (GET) without pagination', async () => {
+    const response = await request(app.getHttpServer()).get('/enrollment');
+    return response.statusCode === HttpStatus.BAD_REQUEST;
+  });
+
+  it('/enrollment (GET) with wrong pagination', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/enrollment')
+      .send({
+        page: 0,
+        count: 0,
+      });
+    return response.statusCode === HttpStatus.BAD_REQUEST;
   });
 });
